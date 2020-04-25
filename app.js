266019a2f4a94bfcb043404c958ae3d7a5993c6f
Nazim Uddin <umnazim@gmail.com>
2020-04-24 23:38:36 -0700

    implement passport authentication and log out

--- app.js
+++ app.js
@@ -59,7 +59,33 @@ app.get("/register", function (req, res) {
   res.render("register");
 });
 
+app.get("/secrets", function (req, res) {
+  if (req.isAuthenticated()) {
+    res.render("secrets");
+  } else {
+    res.redirect("/login");
+  }
+});
+
+app.get("/logout", function (req, res) {
+  req.logout();
+  res.redirect("/");
+});
+
 app.post("/register", function (req, res) {
+  User.register({ username: req.body.username }, req.body.password, function (
+    err,
+    user
+  ) {
+    if (err) {
+      console.log(err);
+      res.redirect("/register");
+    } else {
+      passport.authenticate("local")(req, res, function () {
+        res.redirect("/secrets");
+      });
+    }
+  });
   // bcrypt.hash(req.body.password, saltRounds, function (err, hash) {
   //   const newUser = new User({
   //     email: req.body.username,
@@ -75,6 +101,20 @@ app.post("/register", function (req, res) {
 //   });
 // });
 app.post("/login", function (req, res) {
+  const user = new User({
+    username: req.body.username,
+    password: req.body.password,
+  });
+
+  req.login(user, function (err) {
+    if (err) {
+      console.log(err);
+    } else {
+      passport.authenticate("local")(req, res, function () {
+        res.redirect("secrets");
+      });
+    }
+  });
   // const username = req.body.username;
   // const password = req.body.password;
   // User.findOne({ email: username }, function (err, foundUser) {
